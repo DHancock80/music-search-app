3e40d34fce5cc241f724fa540515829657f5cd92
DHancock80 <tomtom01@gmail.com>
2025-05-12 22:12:23 +0100

    Update music_search_app.py

--- music_search_app.py
+++ music_search_app.py
@@ -188,12 +188,10 @@ if search_query:
                     st.markdown(f"**Artist:** {display_artist}")
 
             with st.expander("Click to view tracklist", expanded=False):
-                tracklist = group[['Track Title', 'Artist', 'CD', 'Track Number']].copy()
-                tracklist = tracklist.rename(columns={
-                    'Track Title': 'Song',
-                    'CD': 'Disc',
-                    'Track Number': 'Track'
-                })
-                if is_compilation:
-                    tracklist['Artist'] = group['Artist'].values
-                st.dataframe(tracklist, use_container_width=True, hide_index=True)
+tracklist = group[['Track Title', 'Artist', 'CD', 'Track Number']].copy()
+tracklist = tracklist.rename(columns={
+    'Track Title': 'Song',
+    'CD': 'Disc',
+    'Track Number': 'Track'
+})
+st.dataframe(tracklist, use_container_width=True, hide_index=True)
