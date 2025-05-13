d2a4369805e5dd5dfa43fdb1a0ffb2487afce4ec
DHancock80 <tomtom01@gmail.com>
2025-05-13 16:11:44 +0100

    Update music_search_app.py

--- music_search_app.py
+++ music_search_app.py
@@ -7,7 +7,7 @@ import base64
 from datetime import datetime
 
 # Constants
-CSV_FILE = 'expanded_discogs_tracklist.csv'
+CSV_FILE = 'expanded_discogs_tracklists.csv'
 COVER_OVERRIDES_FILE = 'cover_overrides.csv'
 DISCOGS_API_TOKEN = st.secrets["DISCOGS_API_TOKEN"]
 GITHUB_TOKEN = st.secrets["GITHUB_TOKEN"]
